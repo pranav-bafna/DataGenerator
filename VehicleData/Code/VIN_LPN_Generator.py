e14600b2110225213b218e4b81088bae5a8aa6d5
pranav-bafna <46337455+pranav-bafna@users.noreply.github.com>
2019-02-01 11:36:21 +0530

    Add files via upload

--- VehicleData/Code/VIN_LPN_Generator.py
+++ VehicleData/Code/VIN_LPN_Generator.py
@@ -6,67 +6,62 @@ import csv
 from collections import OrderedDict
 numpy.random.seed(int(time.time()))
 
-def VIN_LPN_Gen():
-    VIN_LPN_list=[]
-    digits_unique = []
-    letters_unique = []
-    num_records = 5000000
-    file_path = "Unique_VIN_Data1.csv"
-    headers = ["VIN", "License Plate No"]
+VIN_LPN_list=[]
+num_records = 1000000
+file_path = "Unique_VIN_Data1.csv"
+headers = ["VIN", "License Plate No"]
 
-    #Generate Unique Digits
-    x = list(str(i) for i in range(1,10000))
-    digits = [''.join(i) for i in x]
-    for d in range(len(digits)):
-        L = 4-len(digits[d])
-        digits[d] = L*'0'+digits[d]
-        #Get only those numbers with nothing repeated
-        if(digits[d] == ''.join(list(OrderedDict.fromkeys(digits[d])))):
-            digits_unique.append(digits[d])
-    len(digits_unique)
+x = list(str(i) for i in range(1,10000))
+digits = [''.join(i) for i in x]
+digits_unique = []
+for d in range(len(digits)):
+    L = 4-len(digits[d])
+    digits[d] = L*'0'+digits[d]
+    
+    #Get only those numbers with nothing repeated
+    if(digits[d] == ''.join(list(OrderedDict.fromkeys(digits[d])))):
+        digits_unique.append(digits[d])
+   
+len(digits_unique)
 
-    #Generate Unique Characters
-    x = list(chr(i) for i in range(65,91))
-    y = list(chr(i) for i in range(65,91))
-    transpose1 = numpy.transpose([numpy.tile(x, len(y)), numpy.repeat(y, len(x))])
-    temp = [''.join(list(i)) for i in transpose1]
-    transpose2 = numpy.transpose([numpy.tile(x, len(temp)), numpy.repeat(temp, len(x))])
-    letters = [''.join(list(i))+' ' for i in transpose2]
-    #Get only those letter sequences with nothing repeated
-    for L in range(len(letters)):
-        if(letters[L] == ''.join(list(OrderedDict.fromkeys(letters[L])))):
-            letters_unique.append(letters[L])
-    len(letters_unique)
+x = list(chr(i) for i in range(65,91))
+y = list(chr(i) for i in range(65,91))
+#y = list(str(i) for i in range(1,100))
+transpose1 = numpy.transpose([numpy.tile(x, len(y)), numpy.repeat(y, len(x))])
+temp = [''.join(list(i)) for i in transpose1]
+transpose2 = numpy.transpose([numpy.tile(x, len(temp)), numpy.repeat(temp, len(x))])
+letters = [''.join(list(i))+' ' for i in transpose2]
 
-    #Combine digits and characters to generate license plate
-    a = time.time()
-    plates = numpy.transpose([numpy.tile(letters_unique, len(digits_unique)), numpy.repeat(digits_unique, len(letters_unique))])
-    b = time.time()
-    print ('cartesian join took',b-a,'secs')
-    indices = numpy.random.choice(range(len(plates)),num_records, replace=False)
-    c = time.time()
-    print ('random indices took',c-b,'secs')
-    plates_5m = [''.join(i) for i in plates[indices]]
-    d = time.time()
-    print ('join took',d-c,'secs')
 
-    #Generate VIN
-    i=0
-    while(i<num_records):
-        VIN = str(uuid.uuid4()).upper().replace("-","")[0:16]
-        VIN_LPN_list.append([VIN,plates_5m[i]])
-        i = i+1
+letters_unique = []
+#Get only those letter sequences with nothing repeated
+for L in range(len(letters)):
+    if(letters[L] == ''.join(list(OrderedDict.fromkeys(letters[L])))):
+        letters_unique.append(letters[L])
 
-    writer = csv.writer(open(file_path, 'w', newline = '\n'))
-    writer.writerow(headers)
+len(letters_unique)
 
-    with open(file_path, 'a, 'newline = '\n') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(VIN_LPN_list)
-        writeFile.close()
+a = time.time()
+plates = numpy.transpose([numpy.tile(letters_unique, len(digits_unique)), numpy.repeat(digits_unique, len(letters_unique))])
+b = time.time()
+print ('cartesian join took',b-a,'secs')
+indices = numpy.random.choice(range(len(plates)),num_records, replace=False)
+c = time.time()
+print ('random indices took',c-b,'secs')
+plates_5m = [''.join(i) for i in plates[indices]]
+d = time.time()
+print ('join took',d-c,'secs')
 
-def main():
-    VIN_LPN_Gen()
+i=0
+while(i<num_records):
+    VIN = str(uuid.uuid4()).upper().replace("-","")[0:16]
+    VIN_LPN_list.append([VIN,plates_5m[i]])
+    i = i+1
 
-if __name__ == "__main__":
-	main()
+writer = csv.writer(open(file_path, 'w'))
+writer.writerow(headers)
+
+with open(file_path, 'a') as writeFile:
+    writer = csv.writer(writeFile)
+    writer.writerows(VIN_LPN_list)
+    writeFile.close()
